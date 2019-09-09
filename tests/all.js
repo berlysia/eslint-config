2306ab7be0e05846ef512f4df927df49fe06c2ca
berlysia <berlysia@gmail.com>
2019-09-10 02:31:22 +0900

    chore: uncomment

--- tests/all.js
+++ tests/all.js
@@ -2,8 +2,7 @@ module.exports = {
   extends: [
     "../index.js",
     "../jest.js",
-    /* eslint-find-rulesがscopedなpluginに対応していない */
-    // "../typescript.js",
+    "../typescript.js",
     "../flowtype.js",
     "../react.js",
   ],
