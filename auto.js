b4ef0ac115ad8aefbb2afae21925f3df9de50f03
berlysia <berlysia@gmail.com>
2019-10-31 05:27:59 +0900

    fix: make it opt-in the rules with type

--- auto.js
+++ auto.js
@@ -10,13 +10,27 @@ const hasIn = (key, name) => {
 };
 const keys = ["dependencies", "devDependencies", "peerDependencies"];
 const hasInDeps = name => keys.some(k => hasIn(k, name));
+const flatMap = (arr, fn) => arr.reduce((a, i) => a.concat(fn(i)), []);
 
 module.exports = {
   extends: [
-    "@berlysia",
-    hasInDeps("jest") && "@berlysia/eslint-config/jest",
-    hasInDeps("typescript") && "@berlysia/eslint-config/typescript",
-    hasInDeps("flow-bin") && "@berlysia/eslint-config/flowtype",
-    hasInDeps("react") && "@berlysia/eslint-config/react",
+    "./",
+    hasInDeps("flow-bin") && "./flowtype",
+    hasInDeps("react") && "./react",
+  ].filter(Boolean),
+  overrides: [
+    hasInDeps("jest") && {
+      files: flatMap(["test", "spec"], testName =>
+        flatMap(
+          ["js", "ts", "jsx", "tsx"],
+          extName => `*.${testName}.${extName}`
+        )
+      ),
+      extends: "./jest",
+    },
+    hasInDeps("typescript") && {
+      files: flatMap(["ts", "tsx"], extName => `*.${extName}`),
+      extends: "./typescript-without-type",
+    },
   ].filter(Boolean),
 };
