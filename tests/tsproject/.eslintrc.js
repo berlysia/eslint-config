d7e988b0a1704a3d8fc41548b7481defc1020ee4
berlysia <berlysia@gmail.com>
2022-09-11 13:28:11 +0900

    projtestのエラーを解消

--- tests/tsproject/.eslintrc.js
+++ tests/tsproject/.eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json",
+    tsconfigRootDir: __dirname,
+  },
   extends: ["../../auto.js"],
 };
--- tests/tsproject/src/add.test.ts
+++ tests/tsproject/src/add.test.ts
@@ -1,4 +1,4 @@
-const { add } = require("./add");
+import { add } from "./add";
 
 test("add", () => {
   expect(add(2, 3)).toBe(5);
